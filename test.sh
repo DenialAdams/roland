ab7eb063334fc36c939d2f90a18bb82fe6f55f8e
Richard McCormack <brick@brick.codes>
2020-06-26 19:56:12 -0400

    make test thingy work relatively

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-cargo build --release && cargo run --release --bin roland_test_runner -- tests/ "C:\Users\brick\roland\target\release\rolandc.exe"
+cargo build --release && ./target/release/roland_test_runner.exe tests/ `cygpath -d --absolute ./target/release/rolandc.exe`
