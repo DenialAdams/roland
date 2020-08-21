cfb8bce91259fe70e9e08f82c7a72a548d19eac9
Richard McCormack <brick@brick.codes>
2020-08-21 19:02:00 -0400

    buffer inside

--- rolandc_wasm/index.js
+++ rolandc_wasm/index.js
@@ -16,7 +16,7 @@ window.compileUpdateAll = async function compileUpdateAll() {
    let wasm_bytes = compile_and_update_all();
    if (wasm_bytes != null) {
       output_frame.textContent = "Executing...";
-      let response = new Response(wasm_bytes);
+      let response = new Response(wasm_bytes.prototype.buffer);
       let exec_result = await WebAssembly.instantiateStreaming(response);
    }
 };
