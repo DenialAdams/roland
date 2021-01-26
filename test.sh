8662e3f82d5a9c4340e9a58194861712815a4124
Richard McCormack <brick@brick.codes>
2021-01-26 14:27:57 -0500

    all aboard the linux train

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-cargo build --release && ./target/release/roland_test_runner.exe tests/ `cygpath -d --absolute ./target/release/rolandc_cli.exe`
+cargo build --release && ./target/release/roland_test_runner tests/ `readlink --canonicalize ./target/release/rolandc_cli`
