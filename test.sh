b4d87d12441e21d1c7876dac6236d600974aa0d9
Richard McCormack <brick@brick.codes>
2021-02-26 00:59:27 -0500

    resurrect cygwin support on a conditional basis

--- test.sh
+++ test.sh
@@ -1,1 +1,6 @@
-cargo build --release && ./target/release/roland_test_runner tests/ `readlink --canonicalize ./target/release/rolandc_cli`
+if [ $OSTYPE == "cygwin" ];
+then
+   cargo build --release && ./target/release/roland_test_runner.exe tests/ `cygpath -d --absolute ./target/release/rolandc_cli.exe`
+else
+   cargo build --release && ./target/release/roland_test_runner tests/ `readlink --canonicalize ./target/release/rolandc_cli`
+fi
