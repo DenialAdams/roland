e4523b5e174750361fbaf078698c4c8155d4c949
Richard McCormack <brick@brick.codes>
2022-10-21 20:57:27 -0400

    on my laptop lib mode is faster. going with that

--- test.sh
+++ test.sh
@@ -17,15 +17,15 @@ if [[ $DEBUG == "true" ]];
 then
    if [ $OSTYPE == "cygwin" ];
    then
-      ./target/debug/roland_test_runner.exe tests/ --cli `cygpath -d --absolute ./target/debug/rolandc_cli.exe` $OVERWRITE_PARAMS
+      ./target/debug/roland_test_runner.exe tests/ $OVERWRITE_PARAMS
    else
-      ./target/debug/roland_test_runner tests/ --cli `readlink --canonicalize ./target/debug/rolandc_cli` $OVERWRITE_PARAMS
+      ./target/debug/roland_test_runner tests/ $OVERWRITE_PARAMS
    fi
 else
    if [ $OSTYPE == "cygwin" ];
    then
-      ./target/release/roland_test_runner.exe tests/ --cli `cygpath -d --absolute ./target/release/rolandc_cli.exe` $OVERWRITE_PARAMS
+      ./target/release/roland_test_runner.exe tests/ $OVERWRITE_PARAMS
    else
-      ./target/release/roland_test_runner tests/ --cli `readlink --canonicalize ./target/release/rolandc_cli` $OVERWRITE_PARAMS
+      ./target/release/roland_test_runner tests/ $OVERWRITE_PARAMS
    fi
 fi
