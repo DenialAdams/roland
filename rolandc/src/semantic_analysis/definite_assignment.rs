26ef534bd41991cb883797b2e14ae10fb345d86f
Richard McCormack <brick@brick.codes>
2023-12-17 20:00:55 -0500

    bitsets in definite_assignment

--- rolandc/src/semantic_analysis/definite_assignment.rs
+++ rolandc/src/semantic_analysis/definite_assignment.rs
@@ -1,5 +1,5 @@
-use std::collections::HashSet;
-
+use bitvec::bitbox;
+use bitvec::boxed::BitBox;
 use indexmap::IndexMap;
 
 use crate::error_handling::error_handling_macros::rolandc_error;
@@ -13,12 +13,9 @@ use crate::Program;
 pub fn ensure_variables_definitely_assigned(program: &Program, err_manager: &mut ErrorManager) {
    for procedure in program.procedures.values() {
       if let ProcImplSource::Body(block) = &procedure.proc_impl {
-         // Because we need to clone this fairly frequently, an immutable set is appealing here.
-         // I benchmarked im::HashSet (which is a drop-in replacement) and it seemed slightly worse
-         // for my example, so sticking with std for now. TODO: BitVec?
-         let mut unassigned_vars: HashSet<VariableId> = procedure.locals.keys().copied().collect();
+         let mut unassigned_vars: BitBox = bitbox![1; procedure.locals.len()];
          for param in procedure.definition.parameters.iter() {
-            unassigned_vars.remove(&param.var_id);
+            unassigned_vars.set(procedure.locals.get_index_of(&param.var_id).unwrap(), false);
          }
          ensure_all_variables_assigned_in_block(
             block,
@@ -34,9 +31,9 @@ pub fn ensure_variables_definitely_assigned(program: &Program, err_manager: &mut
 
 fn ensure_all_variables_assigned_in_block(
    block: &BlockNode,
-   unassigned_vars: &mut HashSet<VariableId>,
-   unassigned_vars_after_loop: &mut Option<HashSet<VariableId>>,
-   var_types: &IndexMap<VariableId, ExpressionType>,
+   unassigned_vars: &mut BitBox,
+   unassigned_vars_after_loop: &mut Option<BitBox>,
+   procedure_vars: &IndexMap<VariableId, ExpressionType>,
    pool: &AstPool,
    err_manager: &mut ErrorManager,
 ) {
@@ -45,7 +42,7 @@ fn ensure_all_variables_assigned_in_block(
          stmt_id,
          unassigned_vars,
          unassigned_vars_after_loop,
-         var_types,
+         procedure_vars,
          pool,
          err_manager,
       );
@@ -54,9 +51,9 @@ fn ensure_all_variables_assigned_in_block(
 
 fn ensure_all_variables_assigned_in_stmt(
    stmt_id: StatementId,
-   unassigned_vars: &mut HashSet<VariableId>,
-   unassigned_vars_after_loop: &mut Option<HashSet<VariableId>>,
-   var_types: &IndexMap<VariableId, ExpressionType>,
+   unassigned_vars: &mut BitBox,
+   unassigned_vars_after_loop: &mut Option<BitBox>,
+   procedure_vars: &IndexMap<VariableId, ExpressionType>,
    pool: &AstPool,
    err_manager: &mut ErrorManager,
 ) {
@@ -64,49 +61,57 @@ fn ensure_all_variables_assigned_in_stmt(
    match &sn.statement {
       Statement::VariableDeclaration(_, decl_val, _, var_id) => match decl_val {
          DeclarationValue::Expr(e) => {
-            ensure_expression_does_not_use_unassigned_variable(*e, unassigned_vars, pool, err_manager);
-            unassigned_vars.remove(var_id);
+            ensure_expression_does_not_use_unassigned_variable(*e, unassigned_vars, procedure_vars, pool, err_manager);
+            if let Some(index) = procedure_vars.get_index_of(var_id) {
+               unassigned_vars.set(index, false);
+            }
          }
          DeclarationValue::Uninit => {
-            unassigned_vars.remove(var_id);
+            if let Some(index) = procedure_vars.get_index_of(var_id) {
+               unassigned_vars.set(index, false);
+            }
          }
          DeclarationValue::None => (),
       },
       Statement::Assignment(lhs, rhs) => {
-         ensure_expression_does_not_use_unassigned_variable(*rhs, unassigned_vars, pool, err_manager);
+         ensure_expression_does_not_use_unassigned_variable(*rhs, unassigned_vars, procedure_vars, pool, err_manager);
 
          if let Expression::Variable(var_id) = pool.expressions[*lhs].expression {
-            unassigned_vars.remove(&var_id);
+            if let Some(index) = procedure_vars.get_index_of(&var_id) {
+               unassigned_vars.set(index, false);
+            }
          } else if let Expression::FieldAccess(_, inner_expr_id) = pool.expressions[*lhs].expression {
             if let Expression::Variable(var_id) = pool.expressions[inner_expr_id].expression {
-               if let Some(ExpressionType::Union(_)) = var_types.get(&var_id) {
+               if let Some(ExpressionType::Union(_)) = procedure_vars.get(&var_id) {
                   // Assigning one field of a union fully assigns the variable
-                  unassigned_vars.remove(&var_id);
+                  if let Some(index) = procedure_vars.get_index_of(&var_id) {
+                     unassigned_vars.set(index, false);
+                  }
                }
             }
          }
 
-         ensure_expression_does_not_use_unassigned_variable(*lhs, unassigned_vars, pool, err_manager);
+         ensure_expression_does_not_use_unassigned_variable(*lhs, unassigned_vars, procedure_vars, pool, err_manager);
       }
       Statement::Block(b) => {
          ensure_all_variables_assigned_in_block(
             b,
             unassigned_vars,
             unassigned_vars_after_loop,
-            var_types,
+            procedure_vars,
             pool,
             err_manager,
          );
       }
       Statement::IfElse(cond, then, otherwise) => {
-         ensure_expression_does_not_use_unassigned_variable(*cond, unassigned_vars, pool, err_manager);
+         ensure_expression_does_not_use_unassigned_variable(*cond, unassigned_vars, procedure_vars, pool, err_manager);
 
          let mut else_unassigned = unassigned_vars.clone();
          ensure_all_variables_assigned_in_block(
             then,
             unassigned_vars,
             unassigned_vars_after_loop,
-            var_types,
+            procedure_vars,
             pool,
             err_manager,
          );
@@ -114,47 +119,39 @@ fn ensure_all_variables_assigned_in_stmt(
             *otherwise,
             &mut else_unassigned,
             unassigned_vars_after_loop,
-            var_types,
+            procedure_vars,
             pool,
             err_manager,
          );
 
-         // Optimization: ensure that we union elements from smaller set to larger
-         if unassigned_vars.len() < else_unassigned.len() {
-            std::mem::swap(unassigned_vars, &mut else_unassigned);
-         }
-
-         // Union the then and else results
-         unassigned_vars.extend(else_unassigned);
+         *unassigned_vars |= else_unassigned;
       }
       Statement::Loop(b) => {
-         let mut unassigned_after_new_loop = Some(HashSet::with_capacity(unassigned_vars.capacity()));
+         let mut unassigned_after_new_loop = Some(bitbox![0; procedure_vars.len()]);
          ensure_all_variables_assigned_in_block(
             b,
             unassigned_vars,
             &mut unassigned_after_new_loop,
-            var_types,
+            procedure_vars,
             pool,
             err_manager,
          );
          *unassigned_vars = unassigned_after_new_loop.unwrap();
       }
       Statement::Expression(e) => {
-         ensure_expression_does_not_use_unassigned_variable(*e, unassigned_vars, pool, err_manager);
+         ensure_expression_does_not_use_unassigned_variable(*e, unassigned_vars, procedure_vars, pool, err_manager);
          if *pool.expressions[*e].exp_type.as_ref().unwrap() == ExpressionType::Never {
-            unassigned_vars.clear();
+            unassigned_vars.fill(false);
          }
       }
       Statement::Return(e) => {
-         ensure_expression_does_not_use_unassigned_variable(*e, unassigned_vars, pool, err_manager);
-         unassigned_vars.clear();
+         ensure_expression_does_not_use_unassigned_variable(*e, unassigned_vars, procedure_vars, pool, err_manager);
+         unassigned_vars.fill(false);
       }
-      Statement::Continue => unassigned_vars.clear(),
+      Statement::Continue => unassigned_vars.fill(false),
       Statement::Break => {
-         unassigned_vars_after_loop
-            .as_mut()
-            .unwrap()
-            .extend(unassigned_vars.drain());
+         *unassigned_vars_after_loop.as_mut().unwrap() |= unassigned_vars.as_bitslice();
+         unassigned_vars.fill(false);
       }
       Statement::For { .. } => unreachable!(),
       Statement::While(_, _) => unreachable!(),
@@ -164,60 +161,63 @@ fn ensure_all_variables_assigned_in_stmt(
 
 fn ensure_expression_does_not_use_unassigned_variable(
    expr_id: ExpressionId,
-   unassigned_vars: &mut HashSet<VariableId>,
+   unassigned_vars: &mut BitBox,
+   procedure_vars: &IndexMap<VariableId, ExpressionType>,
    pool: &AstPool,
    err_manager: &mut ErrorManager,
 ) {
    let en = &pool.expressions[expr_id];
    match &en.expression {
       Expression::Variable(var_id) => {
-         if unassigned_vars.contains(var_id) {
-            rolandc_error!(
-               err_manager,
-               en.location,
-               "Variable may not have been assigned at this use"
-            );
-            // To avoid spamming with errors, pretend it has been assigned
-            unassigned_vars.remove(var_id);
+         if let Some(index) = procedure_vars.get_index_of(var_id) {
+            if unassigned_vars[index] {
+               rolandc_error!(
+                  err_manager,
+                  en.location,
+                  "Variable may not have been assigned at this use"
+               );
+               // To avoid spamming with errors, pretend it has been assigned
+               unassigned_vars.set(index, false);
+            }
          }
       }
       Expression::ProcedureCall { proc_expr, args } => {
-         ensure_expression_does_not_use_unassigned_variable(*proc_expr, unassigned_vars, pool, err_manager);
+         ensure_expression_does_not_use_unassigned_variable(*proc_expr, unassigned_vars, procedure_vars, pool, err_manager);
          for arg in args.iter() {
-            ensure_expression_does_not_use_unassigned_variable(arg.expr, unassigned_vars, pool, err_manager);
+            ensure_expression_does_not_use_unassigned_variable(arg.expr, unassigned_vars, procedure_vars, pool, err_manager);
          }
       }
       Expression::ArrayLiteral(items) => {
          for item in items.iter().copied() {
-            ensure_expression_does_not_use_unassigned_variable(item, unassigned_vars, pool, err_manager);
+            ensure_expression_does_not_use_unassigned_variable(item, unassigned_vars, procedure_vars, pool, err_manager);
          }
       }
       Expression::ArrayIndex { array, index } => {
-         ensure_expression_does_not_use_unassigned_variable(*array, unassigned_vars, pool, err_manager);
-         ensure_expression_does_not_use_unassigned_variable(*index, unassigned_vars, pool, err_manager);
+         ensure_expression_does_not_use_unassigned_variable(*array, unassigned_vars, procedure_vars, pool, err_manager);
+         ensure_expression_does_not_use_unassigned_variable(*index, unassigned_vars, procedure_vars, pool, err_manager);
       }
       Expression::BinaryOperator { operator: _, lhs, rhs } => {
-         ensure_expression_does_not_use_unassigned_variable(*lhs, unassigned_vars, pool, err_manager);
-         ensure_expression_does_not_use_unassigned_variable(*rhs, unassigned_vars, pool, err_manager);
+         ensure_expression_does_not_use_unassigned_variable(*lhs, unassigned_vars, procedure_vars, pool, err_manager);
+         ensure_expression_does_not_use_unassigned_variable(*rhs, unassigned_vars, procedure_vars, pool, err_manager);
       }
       Expression::UnaryOperator(_, e) => {
-         ensure_expression_does_not_use_unassigned_variable(*e, unassigned_vars, pool, err_manager);
+         ensure_expression_does_not_use_unassigned_variable(*e, unassigned_vars, procedure_vars, pool, err_manager);
       }
       Expression::IfX(a, b, c) => {
-         ensure_expression_does_not_use_unassigned_variable(*a, unassigned_vars, pool, err_manager);
-         ensure_expression_does_not_use_unassigned_variable(*b, unassigned_vars, pool, err_manager);
-         ensure_expression_does_not_use_unassigned_variable(*c, unassigned_vars, pool, err_manager);
+         ensure_expression_does_not_use_unassigned_variable(*a, unassigned_vars, procedure_vars, pool, err_manager);
+         ensure_expression_does_not_use_unassigned_variable(*b, unassigned_vars, procedure_vars, pool, err_manager);
+         ensure_expression_does_not_use_unassigned_variable(*c, unassigned_vars, procedure_vars, pool, err_manager);
       }
       Expression::StructLiteral(_, fields) => {
          for e in fields.iter().flat_map(|x| x.1) {
-            ensure_expression_does_not_use_unassigned_variable(*e, unassigned_vars, pool, err_manager);
+            ensure_expression_does_not_use_unassigned_variable(*e, unassigned_vars, procedure_vars, pool, err_manager);
          }
       }
       Expression::FieldAccess(_, e) => {
-         ensure_expression_does_not_use_unassigned_variable(*e, unassigned_vars, pool, err_manager);
+         ensure_expression_does_not_use_unassigned_variable(*e, unassigned_vars, procedure_vars, pool, err_manager);
       }
       Expression::Cast { expr, .. } => {
-         ensure_expression_does_not_use_unassigned_variable(*expr, unassigned_vars, pool, err_manager);
+         ensure_expression_does_not_use_unassigned_variable(*expr, unassigned_vars, procedure_vars, pool, err_manager);
       }
       Expression::BoolLiteral(_) => (),
       Expression::StringLiteral(_) => (),
