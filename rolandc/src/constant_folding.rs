8efeae995b2b12f37f409d65ed5f10fdb2f74b3c
Richard McCormack <brick@brick.codes>
2024-08-07 02:38:04 -0400

    etest to elock down the fact we don't fold away side effects when pointer to ZST

--- rolandc/src/constant_folding.rs
+++ rolandc/src/constant_folding.rs
@@ -640,6 +640,7 @@ fn fold_expr_inner(
             ) == 0
             && matches!(expr_type, ExpressionType::Pointer(_))
          {
+            debug_assert!(!expression_could_have_side_effects(*expr, &folding_context.ast.expressions));
             return Some(Expression::IntLiteral {
                val: 0,
                synthetic: true,
