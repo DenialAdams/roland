25b1fbb6e20c999bcde3af752091f9718dac0e9e
Richard McCormack <brick@brick.codes>
2024-12-24 02:17:07 -0500

    fix cf issue with when + zst pointer

--- rolandc/src/constant_folding.rs
+++ rolandc/src/constant_folding.rs
@@ -673,11 +673,8 @@ fn fold_expr_inner(
                folding_context.target,
             ) == 0
             && matches!(expr_type, ExpressionType::Pointer(_))
+            && !expression_could_have_side_effects(*expr, &folding_context.ast.expressions)
          {
-            debug_assert!(!expression_could_have_side_effects(
-               *expr,
-               &folding_context.ast.expressions
-            ));
             return Some(Expression::IntLiteral {
                val: 0,
                synthetic: true,
