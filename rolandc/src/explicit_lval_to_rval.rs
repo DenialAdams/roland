e3ead93e84e9f4c48aa366f5e09dc8274dba0650
Richard McCormack <brick@brick.codes>
2025-05-26 08:41:23 -0400

    whoops missing these

---
+++ rolandc/src/explicit_lval_to_rval.rs
@@ -0,0 +1,125 @@
+use crate::Program;
+use crate::parse::{
+   AstPool, BlockNode, Expression, ExpressionId, ExpressionNode, ExpressionPool, Statement, StatementId, UnOp,
+};
+use crate::type_data::ExpressionType;
+
+pub fn make_lval_to_rval_explicit(program: &mut Program) {
+   for body in program.procedure_bodies.iter() {
+      do_block(&body.1.block, &mut program.ast);
+   }
+   for a_static_expr in program.non_stack_var_info.iter().filter_map(|x| x.1.initializer) {
+      do_expr(a_static_expr, &mut program.ast.expressions, false);
+   }
+}
+
+fn do_block(b: &BlockNode, ast: &mut AstPool) {
+   for s in b.statements.iter().copied() {
+      do_stmt(s, ast);
+   }
+}
+
+fn do_stmt(s: StatementId, ast: &mut AstPool) {
+   let stmt = std::mem::replace(&mut ast.statements[s].statement, Statement::Break);
+   match &stmt {
+      Statement::Assignment(lhs, rhs) => {
+         do_expr(*lhs, &mut ast.expressions, true);
+         do_expr(*rhs, &mut ast.expressions, false);
+      }
+      Statement::Expression(ex) | Statement::Return(ex) => do_expr(*ex, &mut ast.expressions, false),
+      Statement::IfElse {
+         cond, then, otherwise, ..
+      } => {
+         do_expr(*cond, &mut ast.expressions, false);
+         do_block(then, ast);
+         do_stmt(*otherwise, ast);
+      }
+      Statement::Block(bn) | Statement::Loop(bn) => do_block(bn, ast),
+      Statement::Continue | Statement::Break => (),
+      Statement::For { .. } | Statement::While(_, _) | Statement::VariableDeclaration { .. } | Statement::Defer(_) => {
+         unreachable!()
+      }
+   }
+   ast.statements[s].statement = stmt;
+}
+
+fn do_expr(e: ExpressionId, ast: &mut ExpressionPool, is_lhs_context: bool) {
+   let expr_location = ast[e].location;
+   let mut the_expr = std::mem::replace(&mut ast[e].expression, Expression::UnitLiteral);
+   let mut deref_with_rval_child = false;
+   match &the_expr {
+      Expression::ProcedureCall { proc_expr, args } => {
+         do_expr(*proc_expr, ast, false);
+         for arg in args.iter() {
+            do_expr(arg.expr, ast, false);
+         }
+      }
+      Expression::ArrayIndex { array, index } => {
+         do_expr(*array, ast, true);
+         do_expr(*index, ast, false);
+      }
+      Expression::BinaryOperator { lhs, rhs, .. } => {
+         do_expr(*lhs, ast, false);
+         do_expr(*rhs, ast, false);
+      }
+      Expression::UnaryOperator(unop, ex) => {
+         deref_with_rval_child = *unop == UnOp::Dereference && !ast[*ex].expression.is_lvalue_disregard_consts(ast);
+         do_expr(*ex, ast, *unop == UnOp::AddressOf || *unop == UnOp::Dereference);
+      }
+      Expression::FieldAccess(_, base) => do_expr(*base, ast, true),
+      Expression::Cast { expr, .. } => {
+         do_expr(*expr, ast, false);
+      }
+      Expression::IfX(a, b, c) => {
+         do_expr(*a, ast, false);
+         do_expr(*b, ast, false);
+         do_expr(*c, ast, false);
+      }
+      Expression::ArrayLiteral(exprs) => {
+         for expr in exprs.iter().copied() {
+            do_expr(expr, ast, false);
+         }
+      }
+      Expression::StructLiteral(_, exprs) => {
+         for expr in exprs.values().flatten().copied() {
+            do_expr(expr, ast, false);
+         }
+      }
+      Expression::Variable(_)
+      | Expression::EnumLiteral(_, _)
+      | Expression::BoundFcnLiteral(_, _)
+      | Expression::BoolLiteral(_)
+      | Expression::StringLiteral(_)
+      | Expression::IntLiteral { .. }
+      | Expression::FloatLiteral(_)
+      | Expression::UnitLiteral => (),
+      Expression::UnresolvedVariable(_)
+      | Expression::UnresolvedStructLiteral(_, _, _)
+      | Expression::UnresolvedEnumLiteral(_, _)
+      | Expression::UnresolvedProcLiteral(_, _) => unreachable!(),
+   }
+   if let Expression::UnaryOperator(UnOp::AddressOf, child_id) = the_expr {
+      ast[e].location = ast[child_id].location;
+      the_expr = std::mem::replace(&mut ast[child_id].expression, Expression::UnitLiteral);
+   } else if deref_with_rval_child {
+      if is_lhs_context {
+         // make_address()~ = 10 => make_address() = 10
+         let Expression::UnaryOperator(UnOp::Dereference, child_id) = the_expr else {
+            unreachable!();
+         };
+         ast[e].location = ast[child_id].location;
+         ast[e].exp_type = Some(ExpressionType::Pointer(Box::new(ast[e].exp_type.take().unwrap())));
+         the_expr = std::mem::replace(&mut ast[child_id].expression, Expression::UnitLiteral);
+      }
+   } else if is_lhs_context {
+      ast[e].exp_type = Some(ExpressionType::Pointer(Box::new(ast[e].exp_type.take().unwrap())));
+   } else if the_expr.is_lvalue_disregard_consts(ast) {
+      let new_child = ast.insert(ExpressionNode {
+         expression: the_expr,
+         exp_type: Some(ExpressionType::Pointer(Box::new(ast[e].exp_type.clone().unwrap()))),
+         location: expr_location,
+      });
+      the_expr = Expression::UnaryOperator(UnOp::Dereference, new_child);
+   }
+   ast[e].expression = the_expr;
+}
---
+++ rolandc/src/lower_aggregate_access.rs
@@ -0,0 +1,99 @@
+use crate::parse::{BinOp, Expression, ExpressionId, ExpressionNode};
+use crate::size_info::sizeof_type_mem;
+use crate::type_data::{ExpressionType, IntType, IntWidth};
+use crate::{Program, Target};
+
+pub fn lower_aggregate_access(program: &mut Program, target: Target) {
+   let expression_ids: Vec<ExpressionId> = program.ast.expressions.keys().collect();
+   for e in expression_ids {
+      match &program.ast.expressions[e].expression {
+         Expression::FieldAccess(f, base) => {
+            let f = *f;
+            let base = *base;
+            let mem_offset = match program.ast.expressions[base]
+               .exp_type
+               .as_ref()
+               .unwrap()
+               .get_type_or_type_being_pointed_to()
+            {
+               ExpressionType::Struct(s, _) => program
+                  .user_defined_types
+                  .struct_info
+                  .get(*s)
+                  .unwrap()
+                  .size
+                  .as_ref()
+                  .unwrap()
+                  .field_offsets_mem
+                  .get(&f)
+                  .copied()
+                  .unwrap(),
+               ExpressionType::Union(_, _) => 0,
+               _ => unreachable!(),
+            };
+            if mem_offset == 0 {
+               program.ast.expressions[e].expression = program.ast.expressions[base].expression.clone();
+            } else {
+               let offset_node = program.ast.expressions.insert(ExpressionNode {
+                  exp_type: Some(ExpressionType::Int(IntType {
+                     signed: false,
+                     width: IntWidth::Pointer,
+                  })),
+                  expression: Expression::IntLiteral {
+                     val: u64::from(mem_offset),
+                     synthetic: true,
+                  },
+                  location: program.ast.expressions[e].location,
+               });
+               program.ast.expressions[e].expression = Expression::BinaryOperator {
+                  operator: BinOp::Add,
+                  lhs: base,
+                  rhs: offset_node,
+               };
+            }
+         }
+         Expression::ArrayIndex { array, index } => {
+            let array = *array;
+            let index = *index;
+            let sizeof_inner = match program.ast.expressions[array]
+               .exp_type
+               .as_ref()
+               .unwrap()
+               .get_type_or_type_being_pointed_to()
+            {
+               ExpressionType::Array(x, _) => sizeof_type_mem(x, &program.user_defined_types, target),
+               _ => unreachable!(),
+            };
+            let sizeof_literal_node = program.ast.expressions.insert(ExpressionNode {
+               exp_type: Some(ExpressionType::Int(IntType {
+                  signed: false,
+                  width: IntWidth::Pointer,
+               })),
+               expression: Expression::IntLiteral {
+                  val: u64::from(sizeof_inner),
+                  synthetic: true,
+               },
+               location: program.ast.expressions[e].location,
+            });
+            let mul_node = program.ast.expressions.insert(ExpressionNode {
+               exp_type: Some(ExpressionType::Int(IntType {
+                  signed: false,
+                  width: target.lowered_ptr_width(),
+               })),
+               expression: Expression::BinaryOperator {
+                  operator: BinOp::Multiply,
+                  lhs: sizeof_literal_node,
+                  rhs: index,
+               },
+               location: program.ast.expressions[e].location,
+            });
+            program.ast.expressions[e].expression = Expression::BinaryOperator {
+               operator: BinOp::Add,
+               lhs: array,
+               rhs: mul_node,
+            };
+         }
+         _ => (),
+      }
+   }
+}
