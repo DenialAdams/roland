2be45806a365063c482f54699408bd00f4c6c15c
Richard McCormack <brick@brick.codes>
2026-05-15 11:38:01 -0400

    be more careful about importing path semantics

--- rolandc/src/imports.rs
+++ rolandc/src/imports.rs
@@ -165,7 +165,7 @@ pub fn import_program(
          let (new_path, is_std) = if let Some(std_path) = file_str.strip_prefix("std:") {
             (std_path.into(), true)
          } else {
-            (base_path.with_file_name(file_str), node.is_std)
+            (base_path.parent().unwrap().join(file_str), node.is_std)
          };
          import_queue.push(ImportQueueNode {
             path: new_path,
