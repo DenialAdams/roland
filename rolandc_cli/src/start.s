88f21e55a07a291616da2ee79fb42b78de9ca857
Richard McCormack <brick@brick.codes>
2026-09-12 12:58:55 -0400

    best effort turn shadow stacks on for freestanding binaries

--- rolandc_cli/src/start.s
+++ rolandc_cli/src/start.s
@@ -3,6 +3,22 @@
 .type _start, @function
 
 _start:
+    # arch_prctl(ARCH_SHSTK_ENABLE, ARCH_SHSTK_SHSTK)
+    mov $158, %eax
+    mov $0x5001, %edi
+    mov $1, %esi
+    syscall
+
+    test %rax, %rax
+    js .Lcontinue
+
+    # arch_prctl(ARCH_SHSTK_LOCK, ARCH_SHSTK_SHSTK)
+    mov $158, %eax
+    mov $0x5003, %edi
+    mov $1, %esi
+    syscall
+
+.Lcontinue:
     call __roland_entry
     ud2
 
